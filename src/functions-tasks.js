9e455a2cfe8f8b3d66950f626a39cc6e855f490f
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-09-10 17:22:52 +0500

    feat: add solution for getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,11 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.reduce((acc, curr) => {
+    acc.push(curr.length);
+    return acc;
+  }, []);
 }
 
 /**
