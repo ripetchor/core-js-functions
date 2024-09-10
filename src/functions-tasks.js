caa99e934688c8733ec28f7a5203bcf4b3bede87
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-09-10 18:03:31 +0500

    feat: add solution for logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -181,8 +181,18 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+
+    logFunc(`${func.name}(${argsStr}) starts`);
+
+    const result = func(...args);
+
+    logFunc(`${func.name}(${argsStr}) ends`);
+
+    return result;
+  };
 }
 
 /**
